e7d45262abb3819221b2ce435e2585e47c0282bc
Vishal <pal.vishalkumar4@gmail.com>
2023-05-15 20:44:53 +0530

    fixed link

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./styles/app.scss";
 import { createContext } from "react";
 
-export const server = "https://nodejs-todoapp.onrender.com/api/v1";
+export const server = "https://todoapp-pzo7.onrender.com/api/v1";
 
 export const Context = createContext({ isAuthenticated: false });
 
